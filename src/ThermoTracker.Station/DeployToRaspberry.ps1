f27d200641d597e4138711fe85a069cb446143bc
IB38 <evilman1996@gmail.com>
2020-09-19 06:39:33 +0700

    Recreation of service. Added station project with deployment to raspberry pi.

---
+++ src/ThermoTracker.Station/DeployToRaspberry.ps1
@@ -0,0 +1,3 @@
+﻿docker context use remote
+docker build -t thermo-tracker-station-image -f Dockerfile .
+docker create --name thermo-tracker-station thermo-tracker-station-image
---
+++ src/ThermoTracker.Station/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading.Tasks;
+
+namespace ThermoTracker.Station
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using (var display = new Tm1637Display(3, 2, PinNumberingScheme.Logical))
+            {
+                display.TurnedOn = true;
+                display.Brightness = 7;
+                display.DisplayDigit(0);
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                await FlashScreen(display, 5);
+
+                display.TurnedOn = false;
+
+                Console.WriteLine("This is it folks!");
+            }
+        }
+
+        private static async Task FlashScreen(Tm1637Display display, int numberOfFlashes, TimeSpan? delay = null)
+        {
+            if (delay == null)
+                delay = TimeSpan.FromMilliseconds(100);
+
+            var initiallyTurnedOn = display.TurnedOn;
+            for (int i = 0; i < numberOfFlashes; i++)
+            {
+                display.TurnedOn = !display.TurnedOn;
+                await Task.Delay(delay.Value);
+            }
+
+            display.TurnedOn = initiallyTurnedOn;
+
+        }
+    }
+}
